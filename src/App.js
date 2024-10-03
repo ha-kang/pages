780ba619e82544f4224792e27b4b10a59fe1d60d
ha-kang <hakang@mz.co.kr>
2024-10-03 09:11:53 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ const App = () => {
       </header>
       <main>
         <div className="content">
-          <h1>Perfect Way To Buy And Sell A Home</h1>
-          <p>Diam vitae, nec mattis lectus quam pretium amet facilisis. Urna, massa aliqua dui pellentesque.</p>
+          <h1>Cloudflare API Query</h1>
+          <p>API를 조회할 고객사, 엔드포인트, 기간을 선택하세요.</p>
           <SearchForm />
         </div>
         <div className="image">
