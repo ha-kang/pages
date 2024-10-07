3f9b4a09710b53eafceb4ae491662f6b9f984993
ha-kang <hakang@mz.co.kr>
2024-10-07 13:55:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     <div className="app">
       <main>
         <div className="content">
-          <h1>Cloudflare API Query</h1>
+          <h1>Cloudflare API</h1>
           <p>API를 조회할 고객사, 엔드포인트, 기간을 선택하세요.</p>
           <SearchForm />
         </div>
