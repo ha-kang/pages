a5c72f7a7d5efa00ce2a7a794cb3c677e92875a4
ha-kang <hakang@mz.co.kr>
2024-10-12 13:41:12 +0900

    Update SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -160,6 +160,15 @@ const SearchForm = () => {
     label: name
   }));
 
+  const formatBytes = (bytes) => {
+    if (bytes === 0 || bytes === undefined) return '0 B';
+    const k = 1000;
+    const sizes = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    const convertedValue = (bytes / Math.pow(k, i)).toFixed(2);
+    return `${convertedValue} ${sizes[i]} (${bytes})`;
+
+    
   const formatNumber = (number) => {
     if (number === undefined || number === null) return 'N/A';
     if (typeof number === 'string') return number; // Handle error messages
