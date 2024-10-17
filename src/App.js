f8e0f085d2ea7dc9beb728f6c4f5bf79b9b87c2d
ha-kang <hakang@mz.co.kr>
2024-10-17 11:22:59 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -244,33 +244,34 @@ const App = () => {
     return <pre className="result-item">{JSON.stringify(result, null, 2)}</pre>;
   };
 
-return (
-  <div className="app">
-    <main>
-      <div className="content">
-        <h1>Cloudflare API</h1>
-        <p>API를 조회할 고객사, 엔드포인트, 기간을 선택하세요.</p>
-        <SearchForm onResultsReceived={setResults} />
-      </div>
-      <div className="image">
-        {results ? (
-          <div className="results-container">
-            <div className="results-box">
-              <div className="endpoint-results">
-                {Object.entries(results).map(([endpoint, result]) => (
-                  <div key={endpoint} className="result-group">
-                    {renderResult(endpoint, result)}
-                  </div>
-                ))}
+  return (
+    <div className="app">
+      <main>
+        <div className="content">
+          <h1>Cloudflare API</h1>
+          <p>API를 조회할 고객사, 엔드포인트, 기간을 선택하세요.</p>
+          <SearchForm onResultsReceived={setResults} />
+        </div>
+        <div className="image">
+          {results ? (
+            <div className="results-container">
+              <div className="results-box">
+                <div className="endpoint-results">
+                  {Object.entries(results).map(([endpoint, result]) => (
+                    <div key={endpoint} className="result-group">
+                      {renderResult(endpoint, result)}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        ) : (
-          <img src={cloudflareImage} alt="Cloudflare logo" />
-        )}
-      </div>
-    </main>
-  </div>
-);
+          ) : (
+            <img src={cloudflareImage} alt="Cloudflare logo" />
+          )}
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default App;
